b802c101fac50cdb5e04a9ecad93fde39bf2e533
profeCarballeira <carballeira2024@gmx.es>
2025-11-24 09:19:58 +0100

    CLIENTE

--- 06-TCPServer.py
+++ 06-TCPServer.py
@@ -1,16 +1,17 @@
 import socket
-HOST = "127.0.0.1"
+
+HOST = '127.0.0.1' #dirección de loopback standart para localhos
 PORT = 2000
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPv4 - TCP
 try:
-    s.bind((HOST,PORT))
-    s.listen(1)
-    conn, addr = s.accept() #bloqueante
-    print (f"Conexión OK con el cliente. IP {addr[0]} Puerto {addr[1]}")
+    s.bind((HOST, PORT))
+    s.listen(1) #creo una lista de un cliente en espera
+    conn, addr = s.accept() #función bloqueante
+    print (f"Conexión exitosa del cliente. IP {addr[0]} Puerto {addr[1]}")
     conn.close()
 except socket.error as exc:
     print ("Excepción de socket: %s" % exc)
 finally:
+    #cerramos la escucha general de nuestro servidor
     s.close()
